958c8b71ba9c72118e1764ecd141932718e4ef02
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2025-10-02 13:07:12 +0200

    Fix display balance formatting to two decimal places

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,5 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        # Always display two decimal places
+        print(f"Current Balance: ${self.account_balance:.2f}")
